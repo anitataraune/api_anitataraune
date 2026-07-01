8fcac63c0abecddcadbc4480f9fb854e79cf2554
anitataraune <ataraune2022@incos-santacruz.edu.bo>
2026-07-01 01:16:11 -0400

    cambio final

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,83 +5,97 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Resources\ProductResource;
 
 class ProductController extends Controller
 {
     // GET /api/products
     public function index()
     {
-        return response()->json(
-            Product::with('category')->get(),
-            200
-        );
-    }
+        return response()->json([
+            'success' => true,
+            'message' => 'Lista de productos',
+            'data' => ProductResource::collection(
+                Product::with('category')->get()
+                )
+        ]);
+        }
 
     // POST /api/products
     public function store(Request $request)
     {
-        $request->validate([
+        $validated = $request->validate([
             'name' => 'required|max:255',
             'price' => 'required|numeric',
             'category_id' => 'required|exists:categories,id',
             'image' => 'required'
         ]);
-
-        $product = Product::create($request->all());
+        
+        $product = Product::create($validated);
 
         return response()->json([
+            'success' => true,
             'message' => 'Producto creado correctamente',
-            'data' => $product
+            'data' => new ProductResource($product->load('category'))
         ], 201);
     }
 
+
     // GET /api/products/{id}
-    public function show(string $id)
+    public function show( $id)
     {
         $product = Product::with('category')->find($id);
 
         if (!$product) {
             return response()->json([
+                'success' => false,
                 'message' => 'Producto no encontrado'
             ], 404);
         }
 
-        return response()->json($product);
+        return response()->json([
+            'success' => true,
+            'message' => 'Producto encontrado',
+            'data' => new ProductResource($product)
+        ]);
     }
 
     // PUT /api/products/{id}
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
         $product = Product::find($id);
 
         if (!$product) {
             return response()->json([
+                'success' => false,
                 'message' => 'Producto no encontrado'
             ], 404);
         }
 
-        $request->validate([
+       $validated = $request->validate([
             'name' => 'required|max:255',
             'price' => 'required|numeric',
             'category_id' => 'required|exists:categories,id',
             'image' => 'required'
         ]);
 
-        $product->update($request->all());
+        $product->update($validated);
 
         return response()->json([
+            'success' => true,
             'message' => 'Producto actualizado correctamente',
-            'data' => $product
+            'data' => new ProductResource($product)
         ]);
     }
 
     // DELETE /api/products/{id}
-    public function destroy(string $id)
+    public function destroy($id)
     {
         $product = Product::find($id);
 
         if (!$product) {
             return response()->json([
+                'success' => false,
                 'message' => 'Producto no encontrado'
             ], 404);
         }
@@ -89,6 +103,7 @@ class ProductController extends Controller
         $product->delete();
 
         return response()->json([
+            'success' => true,
             'message' => 'Producto eliminado correctamente'
         ]);
     }
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -7,19 +7,19 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
-        return 
-[
+        return [
             'id' => $this->id,
-            'nombre' => $this->nombre,
-            'precio' => $this->precio,
-            'categoria_id' => $this->categoria_id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'image' => $this->image,
+
+            'category' => [
+                'id' => $this->category->id ?? null,
+                'name' => $this->category->name ?? null,
+            ],
+
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
